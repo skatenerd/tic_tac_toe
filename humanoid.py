734c70180efd27f4577ab8114a52720e92685e87
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-06-19 16:00:00 -0500

    Refactor

--- humanoid.py
+++ humanoid.py
@@ -1,15 +1,17 @@
 from ai import AI
 from player import Player
+from playerinput import PlayerInput
 
 class Humanoid(AI):
 
-    def __init__(self,token,input_object):
+    def __init__(self,token,input_object=PlayerInput()):
         super(AI,Humanoid).__init__(self,token,input_object)
         self.times_next_move_called = 0
         self.minimax_status = {"alpha":-1,"beta":1}
+        self.input_object = input_object
  
     def next_move(self,board):
         self.times_next_move_called += 1 
-        if self.input_object and self.times_next_move_called < 4:
+        if self.times_next_move_called < 4:
             return self.input_object.call() 
         return AI.next_move(self,board) 
