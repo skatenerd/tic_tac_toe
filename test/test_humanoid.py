a6492bf9483c43c238958abb4c532e128cd1756e
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-06-20 11:42:39 -0500

    Update

--- test/test_humanoid.py
+++ test/test_humanoid.py
@@ -19,21 +19,15 @@ class HumanoidNextMoveTests(unittest.TestCase):
         self.assertEqual(1,player.next_move(board))
         self.assertEqual(2,player.next_move(board))
 
-    def test_third_move_is_human(self):
-        mock = MockUserInput([1,2,3])
-        player = Humanoid("x",input_object=mock)
-        board = Board()
-        self.assertEqual(1,player.next_move(board))
-        self.assertEqual(2,player.next_move(board))
-        self.assertEqual(3,player.next_move(board))
-
-    def test_that_fourth_move_is_ai(self):
-        mock = MockUserInput([1,2,5])
+    def test_third_move_is_ai(self):
+        mock = MockUserInput([1,7])
         player = Humanoid("x",input_object=mock)
         board = Board()
         board.make_move(4,"o")
-        board.make_move(8,"o") 
-        for i in range(3):
-            player.next_move(board)
-        WINNERS = (3,9) 
-        self.assertTrue(player.next_move(board) in WINNERS) 
+        board.make_move(5,"o") 
+        self.assertEqual(1,player.next_move(board))
+        board.make_move(1,"x") 
+        self.assertEqual(7,player.next_move(board))
+        board.make_move(7,"x") 
+        self.assertEqual(6,player.next_move(board))
+                           
