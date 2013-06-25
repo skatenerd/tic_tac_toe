5db26ee596657c4372f896af263c181fca290b4d
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-06-25 09:55:56 -0500

    Removed scenario class, refactored easy_ai class

--- easy_ai.py
+++ easy_ai.py
@@ -5,9 +5,13 @@ class EasyAI(AI):
 
     def next_move(self,board):
         move_list = self.__build_move_list__(board)
-        easy_move_list = filter(lambda move_list: move_list[0] < 1, move_list)
+        easy_move_list = filter(self.lowest_scores,move_list)
         if easy_move_list:
             move = random.choice(easy_move_list)[1]
         else:
-        	move = random.choice(move_list)[1]
+            move = random.choice(move_list)[1]
         return move
+
+    def lowest_scores(self,move_list):
+        score = move_list[0] 
+	return score <= 0	
