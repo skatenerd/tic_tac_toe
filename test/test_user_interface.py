70e07e7fe90de3dc4cda9e8c686b4ae6d7b7ef65
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2013-07-04 17:33:48 -0500

    WHITESPACE

--- test/test_user_interface.py
+++ test/test_user_interface.py
@@ -12,17 +12,17 @@ class UserInterfaceGameSetupTests(unittest.TestCase):
                        "(4) Humanoid vs AI")
         prompt_four = "Would you like to play as x or o: "
         prompt_three = "Would you like to move first or second (1,2): "
-	return prompt_one,prompt_two,prompt_three,prompt_four
+        return prompt_one,prompt_two,prompt_three,prompt_four
 
     def test_for_scenario_one_prompts(self):
-	scenario = 1
-	prompts = self.return_prompts()
-	self.assert_prompts_present(scenario,*prompts)
+      scenario = 1
+      prompts = self.return_prompts()
+      self.assert_prompts_present(scenario,*prompts)
 
     def test_scenario_four_prompts(self):
-	scenario = 4
-	prompts = self.return_prompts()
-	self.assert_prompts_present(scenario,*prompts)
+      scenario = 4
+      prompts = self.return_prompts()
+      self.assert_prompts_present(scenario,*prompts)
 
     def assert_prompts_present(self,scenario_number,*prompts):
       dummy_input = [scenario_number,1,"x","easy"]
@@ -35,29 +35,29 @@ class UserInterfaceGameSetupTests(unittest.TestCase):
         self.assertTrue(prompt in history_string)
 
     def call_game_setup_with_input_list(self,input_list):
-	fake_printer = FakePrinter()
-	mock = MockUserInput(input_list)
-	ui = UserInterface(mock,fake_printer)
-	return ui.game_setup()
+      fake_printer = FakePrinter()
+      mock = MockUserInput(input_list)
+      ui = UserInterface(mock,fake_printer)
+      return ui.game_setup()
 
 class UserInterfaceShowFlaggedPromptsTests(unittest.TestCase):
 
     def test_that_only_flagged_prompts_are_shown(self):
-        mock = MockUserInput([3,"easy"])
-	fake_printer = FakePrinter()
-	ui = UserInterface(mock,fake_printer)
-	ui.game_setup()
-	# Prompt for choosing scenario is only necessary prompt
-	prompts_necessary_for_scenario_three = 2
-	printed_strings = len(fake_printer.history)
-	self.assertEqual(printed_strings,prompts_necessary_for_scenario_three)
+      mock = MockUserInput([3,"easy"])
+      fake_printer = FakePrinter()
+      ui = UserInterface(mock,fake_printer)
+      ui.game_setup()
+      # Prompt for choosing scenario is only necessary prompt
+      prompts_necessary_for_scenario_three = 2
+      printed_strings = len(fake_printer.history)
+      self.assertEqual(printed_strings,prompts_necessary_for_scenario_three)
 
-	mock = MockUserInput([2])
-	fake_printer = FakePrinter()
-	ui = UserInterface(mock,fake_printer)
-	ui.game_setup()
-	prompts_necessary_for_scenario_two = 1
-	printed_strings = len(fake_printer.history)
-	self.assertEqual(printed_strings,prompts_necessary_for_scenario_two)
+      mock = MockUserInput([2])
+      fake_printer = FakePrinter()
+      ui = UserInterface(mock,fake_printer)
+      ui.game_setup()
+      prompts_necessary_for_scenario_two = 1
+      printed_strings = len(fake_printer.history)
+      self.assertEqual(printed_strings,prompts_necessary_for_scenario_two)
 
 
