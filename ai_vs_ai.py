84741b68159da9d1d3090054aa05ea8d36d4b8a8
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2013-07-05 11:00:45 -0500

    Let user specify AI difficulty

--- ai_vs_ai.py
+++ ai_vs_ai.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from ai import ImpossibleAI
+from easy_ai import EasyAI
 from game import Game
 from no_prompt_interface import NoPromptInterface
 
@@ -12,7 +13,15 @@ class AiVsAiScenario(object):
       self.user_data = user_data
 
     def game(self):
-      return Game(ImpossibleAI('x'), ImpossibleAI('o'))
+      return Game(self.first_ai_class()('x'), ImpossibleAI('o'))
+
+    def first_ai_class(self):
+      difficulty_response = self.user_data[AiVsAiScenario.first_ai_difficulty_prompt()]
+      if difficulty_response == "easy":
+        return EasyAI
+      else:
+        return ImpossibleAI
+
 
     @staticmethod
     def first_ai_difficulty_prompt():
--- test/test_ai_vs_ai.py
+++ test/test_ai_vs_ai.py
@@ -1,10 +1,23 @@
 import unittest
+from ai import ImpossibleAI
+from easy_ai import EasyAI
 from ai_vs_ai import AiVsAiScenario
 
 class AiVsAiScenarioTests(unittest.TestCase):
 
-    def test_that_ai_choice_shows_up(self):
-	prompt = "What difficulty would you like the first ai to be (easy,impossible): " 
-	self.assertTrue(prompt in AiVsAiScenario.prompts())
+    def test_listens_to_easy_difficulty(self):
+      user_data = {
+          "What difficulty would you like the first ai to be (easy,impossible): ":
+          "impossible"
+      }
+      game = AiVsAiScenario(user_data).game()
+      self.assertIsInstance(game.player_one, ImpossibleAI)
 
-    
+    def test_listens_to_easy_difficulty(self):
+      user_data = {
+          "What difficulty would you like the first ai to be (easy,impossible): ":
+          "easy"
+      }
+      game = AiVsAiScenario(user_data).game()
+      self.assertIsInstance(game.player_one, EasyAI)
+
